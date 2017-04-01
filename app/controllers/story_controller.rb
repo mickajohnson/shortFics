39cfba7a858d1acfe1e8523e11e185a6e305e4a2
Mick Johnson <mickajohnson@email.com>
2017-04-01 21:27:34 +0000

    Added proper seeds, tweaked some front end

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -1,6 +1,16 @@
 class StoryController < ApplicationController
+
   def show
     @genre = Genre.find(params[:genre_id])
     @story = Story.find(params[:story_id])
+  end
+
+  def new
+    @genres = Genre.all
+  end
+
+  def create
+    @story = Story.create({title: params[:title], author: params[:author], snippet:params[:snippet], content:params[:content], genre_id:params[:genre]})
+    redirect_to '/genres'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root 'user#new'
   post 'users' => 'user#create'
   get 'genres' => 'genre#index'
-  get 'genres/:genre_id/:snippet_id' => 'genre#snippet'
+  get 'genres/:genre_id/ => 'genre#snippet'
   get 'genres/:genre_id/stories/:story_id' => 'story#show'
   get 'logout' => 'user#logout'
+  get 'stories/new' => 'story#new'
+  post 'stories' => 'story#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
