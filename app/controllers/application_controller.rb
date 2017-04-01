72d2981eaedf63767ab259312c634265c6ada68f
Mick Johnson <mickajohnson@email.com>
2017-04-01 22:10:13 +0000

    Fixed algo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
+  def require_login
+    unless session[:id]
+      redirect_to '/'
+    else
+      @user = User.find(session[:user_id]) if session[:user_id]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'user#new'
   post 'users' => 'user#create'
   get 'genres' => 'genre#index'
-  get 'genres/:genre_id/ => 'genre#snippet'
+  get 'genres/:genre_id/' => 'genre#snippet'
   get 'genres/:genre_id/stories/:story_id' => 'story#show'
   get 'logout' => 'user#logout'
   get 'stories/new' => 'story#new'
